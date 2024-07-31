c88432187dc0e3a59c1202a55ad6462057599d0e
martinasladek <m.sladekova@sussex.ac.uk>
2024-07-31 10:30:34 +0100

    update readme and documentation

--- R/z.R
+++ R/z.R
@@ -1,3 +1,14 @@
+
+#' Convert a vector into Z scores
+#'
+#' @param x A numeric vector
+#'
+#' @return A numeric vector
+#' @export
+#'
+#' @examples
+#' x <- c(1,2,3,4,5)
+#' z(x)
 z <- function(x){
-  (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)
+  (x - mean(x, na.rm = TRUE)) / stats::sd(x, na.rm = TRUE)
 }
